81d5fb5eb31d0fb0021bab12151dfc7e41fbcdde
Ernesto Rivera <ernestoriverasa9@gmail.com>
2024-09-12 20:20:47 -0600

    Creación de mi Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script>
+	import logo1 from "$lib/assets/logos/logo1.png";
+</script>
+
+<nav class=" p-4 mx-8">
+	<div class="flex justify-center">
+		<div class=" flex flex-grow">
+			<a href="/">
+				<img class="flex justify-start" src={logo1} height="65" width="65" alt="logo1" />
+			</a>
+		</div>
+		<div></div>
+		<div>
+			<button class="bg-yellow-500 p-3 rounded flex gap-2"
+				>Pedí y retirá
+				<span class="material-symbols-outlined"> lunch_dining </span>
+			</button>
+		</div>
+	</div>
+</nav>
