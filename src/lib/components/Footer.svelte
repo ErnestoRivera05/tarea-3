45654dbaff4c150979622cfbc195a5e6e19866c7
Ernesto Rivera <ernestoriverasa9@gmail.com>
2024-09-12 20:18:59 -0600

    Creación del footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,17 @@
+<script>
+	import logo1 from "$lib/assets/logos/logo1.png";
+</script>
+
+<footer class="mx-8 p-4 text-lg">
+	<div class="flex">
+		<div class="flex-grow font-light">
+			<a href="/" class="hover:underline">Aviso de Privacidad</a>
+			<a href="/" class="hover:underline">Terminos y condiciones</a>
+			<a href="/" class="hover:underline">Datos Personales</a>
+		</div>
+		<div class="flex gap-2">
+			<img src={logo1} alt="logo2" height="40" width="40" class="" />
+			<h1 class="font-light">McDonald's 2024</h1>
+		</div>
+	</div>
+</footer>
